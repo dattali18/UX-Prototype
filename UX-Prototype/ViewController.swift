c28b8736eb2e1ae5504e5eb5e04308a88f6daa89
dattali18 <74492594+dattali18@users.noreply.github.com>
2023-09-14 11:08:47 +0300

    Initial Commit

---
+++ UX-Prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UX-Prototype
+//
+//  Created by Daniel Attali on 9/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
