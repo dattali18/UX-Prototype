a859615e51771d7af5fed4e0dbb7d1270c3a957a
dattali18 <74492594+dattali18@users.noreply.github.com>
2023-09-19 09:26:50 +0300

    added some login func

--- UX-Prototype/Views/Login/LoginVC/LoginVC.swift
+++ UX-Prototype/Views/Login/LoginVC/LoginVC.swift
@@ -16,12 +16,39 @@ class LoginVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
         // Do any additional setup after loading the view.
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        if(UserLogin.shared.asSignUp()) {
+            let user :User = UserLogin.shared.loadUser()
+            
+            userNameTxtField.text = user.name
+            emailTxtField.text = user.email
+            passTxtField.text = user.password
+        }
+        
     }
 
     @IBAction func signInBtnPressed(_ sender: Any) {
         // check if the user is the user we have created
+        let name = userNameTxtField.text
+        let email = emailTxtField.text
+        let password = passTxtField.text
+        
+        if(name != nil && email != nil && password != nil) {
+            if(UserLogin.shared.signIn(Name: name!, Email: email!, Password: password!)) {
+                
+
+                
+                let storyboard = UIStoryboard (name: "Main", bundle: nil)
+                let CustomTB = storyboard.instantiateViewController(identifier: "MainTB") as! MainTabBarVC
+                
+                (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.setRootViewController(CustomTB)
+                
+            }
+        }
+        
     }
     
 }
